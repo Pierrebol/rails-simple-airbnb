195e752524fe0fa037c6cbd71764c869a947ce5d
pierrebollack <pierre.bollack@edhec.com>
2021-06-09 17:02:06 +0200

    flats controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'Sombre & petit appartement Paris',
+  address: '10 rue de Paris',
+  description: 'Petit, peu de lumière, assez mal placé avec une petite kitchenette et pas de toilette. Un appartement pour ceux qui veulent découvrir le VRAI Paris!',
+  price_per_night: 800,
+  number_of_guests: 1
+)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'Sombre & petit appartement Paris',
+  address: '10 rue de Paris',
+  description: 'Petit, peu de lumière, assez mal placé avec une petite kitchenette et pas de toilette. Un appartement pour ceux qui veulent découvrir le VRAI Paris!',
+  price_per_night: 800,
+  number_of_guests: 1
+)
